43f2cac068bd72f0e5a7594388c2a8cb43e8277c
Camilo Valdes <bolivar014@gmail.com>
2022-09-26 09:31:32 -0500

    creando proyecto baase

---
+++ WEBAPI_RESTFULL/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEBAPI_RESTFULL.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
