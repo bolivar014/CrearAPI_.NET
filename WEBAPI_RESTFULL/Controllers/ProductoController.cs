59b1f43a0305c629fa063c94f1c0c2da78f6e41a
Camilo Valdes <bolivar014@gmail.com>
2022-09-26 11:30:48 -0500

    creando metodo tipo POST - Creación de producto

--- WEBAPI_RESTFULL/Controllers/ProductoController.cs
+++ WEBAPI_RESTFULL/Controllers/ProductoController.cs
@@ -25,11 +25,28 @@ namespace WEBAPI_RESTFULL.Controllers
             return context.Producto.ToList();
         }
 
+        // Visualizar producto por ID
         [HttpGet("{id}")]
         public Producto Get(string id)
         {
             var producto = context.Producto.FirstOrDefault(x => x.pro_codigo == id);
             return producto;
+        }
+
+        // Metodo POST para la creación de producto
+        [HttpPost]
+        public ActionResult Post([FromBody] Producto producto)
+        {
+            // Controlamos ejecución
+            try
+            {
+                context.Producto.Add(producto);
+                context.SaveChanges();
+                return Ok(); // En caso que se agregue exitosamente
+            } catch (Exception ex)
+            {
+                return BadRequest(); // Retornamos en caso que suceda un error
+            }
         }
     }
 }
