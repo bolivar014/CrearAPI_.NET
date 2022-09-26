adc7248f4a4eca0938e6a19415504e1354d9b891
Camilo Valdes <bolivar014@gmail.com>
2022-09-26 11:59:58 -0500

    creando metodo tipo PUT - DELETE

--- WEBAPI_RESTFULL/Controllers/ProductoController.cs
+++ WEBAPI_RESTFULL/Controllers/ProductoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WEBAPI_RESTFULL.Context;
 using WEBAPI_RESTFULL.Entities;
 
@@ -46,6 +47,45 @@ namespace WEBAPI_RESTFULL.Controllers
             } catch (Exception ex)
             {
                 return BadRequest(); // Retornamos en caso que suceda un error
+            }
+        }
+
+        // metodo PUT para la actualización de registro
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Producto producto)
+        {
+            // Validamos si el id enviado existe en la entidad Producto
+            if(producto.pro_codigo == id)
+            {
+                context.Entry(producto).State = EntityState.Modified;
+                context.SaveChanges();
+                return Ok();
+            } else
+            {
+                // En caso que suceda que no exista el ID de actualización
+                return BadRequest();
+            }
+            
+        }
+
+        // metodo DELETE para eliminación de registros
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            // Verificamos si ID existe en objeto producto
+            var producto = context.Producto.FirstOrDefault(p => p.pro_codigo == id);
+
+            // Validamos obj producto
+            if(producto != null)
+            {
+                // En caso que existan registros, eliminamos
+                context.Producto.Remove(producto);
+                context.SaveChanges();
+                return Ok();
+            } else
+            {
+                // En caso que no existan registros.
+                return BadRequest();
             }
         }
     }
